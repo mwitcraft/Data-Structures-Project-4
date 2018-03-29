60c0535dd15548b58a0e883fd16980fa6b1143ad
Mason <mwitcraft11@gmail.com>
2018-03-29 16:36:15 -0500

    Worked on tnode.cpp and ttree.cpp a little bit I think I have insert method in tnode working Worked on insert in ttree but i still think it needs some work

---
+++ tnode.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <list>
+#include <string>
+// #include "ttree.cpp"
+using namespace std;
+
+class ttree;
+
+class tnode{
+private:
+	ttree* _nextLevel; //Pointer to the ttree at the next level
+	list<string>* _words; //Stores keywords
+public:
+	tnode();
+	tnode(tnode& tn);
+	~tnode();
+
+	//ostream operator and other methods
+
+	friend ostream& operator<<(ostream& os, ttree& tt);
+	ttree* getNext();
+	list<string>* getWords();
+	void find(string key, list<string>* results);
+
+	void put(string key);
+	bool insert(string key, int level);
+	void setNext(ttree* nextLevel);
+	void setWords(list<string>* words);
+
+	void operator=(tnode& tn);
+	string operator[](int index);
+
+
+
+
+
+	//MY SHIT
+	void display();
+
+};
+
+
+tnode::tnode(){
+	_nextLevel = NULL;
+	_words = NULL;
+}
+
+tnode::tnode(tnode& tn){
+	_nextLevel = tn.getNext();
+	_words = tn.getWords();
+}
+
+ttree* tnode::getNext(){
+	return _nextLevel;
+}
+
+list<string>* tnode::getWords(){
+	return _words;
+}
+
+void tnode::find(string key, list<string>* results){
+
+}
+
+void tnode::put(string key){
+	if(_words == NULL){
+		_words = new list<string>();
+	}
+	_words->push_back(key);
+}
+
+bool tnode::insert(string key, int level){
+	if(_words == NULL){
+		_words = new list<string>();
+		_words->push_back(key);
+		return true;
+	}
+
+
+
+
+
+	// string compWord = _words->front();
+	string pre1 = key.substr(0, level);
+	string pre2 = _words->front().substr(0, level);
+
+	// for(int i = 0; i < level; ++i){
+	// 	cout << "key @ " << i << ": " << key[i] << endl;
+	// 			cout << "compWord @ " << i << ": " << compWord[i] << endl;
+
+ // 		pre1[i] = key[i];
+	// 	pre2[i] = compWord[i];
+	// }
+
+	// cout << "pre1: " << pre1 << endl;
+	// cout << "pre2: " << pre2 << endl;
+
+
+	if(pre1 == pre2){
+		return false;
+	}
+
+	if(key < _words->front()){
+		_words->push_front(key);
+	}
+	else if(key > _words->back()){
+		_words->push_back(key);
+	}
+	else{
+		list<string>::iterator it = _words->begin();
+		while(it != _words->end()){
+			string low = (*it);
+			++it;
+			string high = (*it);
+			if(key > low && key < high){
+				_words->insert(it, key);
+				return true;
+			}
+		}
+	}
+
+
+
+}
+
+void tnode::setNext(ttree* nextLevel){
+	_nextLevel = nextLevel;
+}
+
+void tnode::setWords(list<string>* words){
+	_words = words;
+}
+
+void tnode::operator=(tnode& tn){
+
+}
+
+string tnode::operator[](int index){
+	list<string>::iterator it = _words->begin();
+	advance(it, index);
+	return (*it);
+}
+
+void tnode::display(){
+	list<string>::iterator it;
+	int i = 0;
+
+	for(it = _words->begin(); it != _words->end(); ++it, ++i){
+		cout << i << ": " << (*it) << endl;
+	}
+}
+
+int main(){
+	tnode* myNode = new tnode();
+
+	myNode->insert("BLACK", 5);
+	myNode->insert("BLACKIRON", 5);
+	myNode->insert("BLACKSMITH", 5);
+	myNode->insert("BLA", 5);
+	myNode->display();
+}
+
+
+
---
+++ ttree.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include "tnode.cpp"
+using namespace std;
+
+// class tnode;
+
+class ttree{
+private:
+	tnode* _tnodes; //Pointer to _tnodes, a tnode array of size 26 will be created in the constructor
+	int _maxDepth; //Max depth allowance
+	int _currentDepth; //Depth of the current node(root is depth 1)
+public:
+	ttree(); //Set the maxDepth to 5 in the empty constructor
+	ttree(int maxDepth); //Constructor with given maxDepth value
+	~ttree();
+	void insert(string key); //Insert 'key' into the tree
+	void search(string key); //Search for 'key' in the tree and print out all matches
+
+	//ostream operator and other methods
+};
+
+ttree::ttree(){
+	_tnodes = new tnode[26];
+	_maxDepth = 5;
+	_currentDepth = 1;
+}
+
+void ttree::insert(string key){
+	ttree* newLevel;
+	int index = key[_currentDepth - 1] - 'A'; //find which tnode we are concerned with, based on _currentDepth and key
+
+	if((*_tnodes)[index].getNext() != NULL){ //if the node in question has a next level
+		//ttree obj
+		(*_tnodes)[index].getNext()->insert(key); //Pass insert call to next level
+	}
+	else{
+			//tnode obj
+		if((*_tnodes)[index].insert(key, _currentDepth) == false){ //Try to insert, if false then common pre > depth
+			if((_currentDepth + 1) > _maxDepth){
+				(*_tnodes)[index].put(key); //put() into _words if we hit the max depth
+			}
+			else{ //We need a new level
+				newLevel = new ttree(_maxDepth, _currentDepth + 1);
+				list<string> oldWords = (*(_tnodes)[index].getWords());
+
+				//NEED TO WRITE THIS PORTION OF CODE
+				//INSERT CODE HERE TO INSERT EACH WORD IN OLDWORDS IN NEWLEVEL RECURSIVLEY
+				if(!oldWords.empty()){
+					string tempWord = oldWords.front();
+					oldWords.pop_front();
+					newLevel->insert(tempWord)
+				}
+
+				newLevel->insert(key); //try again to insert key now that we have more levels; this makes sure multiple new levels will be created if necessary
+				(*_tnodes)[index].setNext(newLevel);
+				(*_tnodes)[index].setWords(NULL);
+			}
+
+		}
+	}
+}
