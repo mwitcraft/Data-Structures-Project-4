3c2078362a638cb12235e2d5ea048fa3be8d8d44
Witcraft <mason.witcraft@ou.edu>
2018-03-29 11:28:50 -0500

    Testing main
    Main can read command from first char in line and input from string
    following command

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+
+int main(){
+	char command;
+	string input;
+
+	while(!cin.eof()){
+		cin >> command;
+		switch(command){
+			case('I'):{
+				cin >> input;
+				cout << "Insert: " << input << endl;
+				break;
+			}
+			case('S'):{
+				cin >> input;
+				cout << "Search: " << input << endl;
+				break;
+			}
+			case('D'):{
+				cout << "Display" << endl;
+				break;
+			}
+		}
+	}
+}
